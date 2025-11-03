ed6b3fdfcfda5f859be8fc1e7ba41204dd68d1c3
Mikhail Palkin <thefireofcoall@gmail.com>
2025-11-03 14:15:44 +0300

    fix: add prod build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,12 @@ module.exports = {
   apps: [
     {
       name: "sliva-bot",
-      script: "index.ts",
-      interpreter: "ts-node",
-      interpreter_args: "--esm",
-      cwd: "/root/sliva-bot",
+      script: "dist/index.js",   
+      cwd: "/root/sliva-bot", 
       watch: false,
       env: {
         NODE_ENV: "production",
-				BOT_TOKEN: "8005106256:AAEBBdjmpBvT3sDzqWB-6D7PoLduKptnn7Y"
+        BOT_TOKEN: "8005106256:AAEBBdjmpBvT3sDzqWB-6D7PoLduKptnn7Y"
       }
     }
   ]
